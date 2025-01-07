9fbacc1504084dcfa8527ea84efdce117e35f40a
Kaue Gomes <knovato00@gmail.com>
2025-01-07 15:37:28 -0300

    change avatar and links

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const img = document.querySelector("#profile img")
 
 if (html.classList.contains('light')){
 
-img.setAttribute('src', './assets/avatar-light.png') 
+img.setAttribute('src', './assets/avatar-new1.png') 
 
 
 } else {
 
-  img.setAttribute('src', './assets/avatar.png')
+  img.setAttribute('src', './assets/avatar-new1.png')
 }
 
 }
