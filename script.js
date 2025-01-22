21f4686f2eb715f87f90e9dccddd842598b1c519
Kaue Gomes <knovato00@gmail.com>
2025-01-22 16:22:54 -0300

    att2201

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function togglemode() {
-  const html = document.documentElement;
+  const html = document.documentElement
   html.classList.toggle("light")
 
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-new3.png")
+    img.setAttribute(
+      "src",
+      "./assets/avatar-new3.png?v=" + new Date().getTime()
+    )
   } else {
-    img.setAttribute("src", "./assets/avatar-new1.png")
+    img.setAttribute(
+      "src",
+      "./assets/avatar-new1.png?v=" + new Date().getTime()
+    )
   }
 }
